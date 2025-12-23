df05a901ba9b339d0b910c988ebda45cfe0cefd7
sabri-rihab <rihabsabri21@gmail.com>
2025-12-23 22:27:23 +0100

    diagramme de classe

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+echo "Enter your name: ";
+$name = trim(fgets(STDIN));
+
+echo "Hello, $name\n";
